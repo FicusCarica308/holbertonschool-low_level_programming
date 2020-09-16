b5e19d5fa98d3427748767c34cbc0c06b55267dd
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-16 17:22:03 +0000

    This commit changes some formatting on the 5-printf.c script to adhere to the betty coding style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ *main - This program prints a string using the printf function then ends
+ *Return - returns 0 when finished
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
