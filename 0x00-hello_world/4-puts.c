7eb2776d4c63430d389ba0a5c03b99ecf9eed715
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-16 17:16:48 +0000

    This commit changes some formatting on the 4-puts.c script to adhere to the betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-
-int main(void)
+/**
+ * main - main in this code uses the function puts to print a string
+ */
+void main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 }
