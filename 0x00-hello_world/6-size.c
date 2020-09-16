368bbafa2918b5ef720a892205e5f515ef8c86c2
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-16 18:55:33 +0000

    adds typo fix in script 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,12 @@ int main(void)
 {
 	int charSz = sizeof(char);
 	int intSz = sizeof(int);
-	int longIntSz = sizeof(long);
+	int longIntSz = sizeof(long int);
 	int dbleIntSz = sizeof(long long int);
 	int floatSz = sizeof(float);
 
 	printf("Size of a char: %d byte(s)\n", charSz);
-	printf("Size of a int: %d byte(s)\n", intSz);
+	printf("Size of an int: %d byte(s)\n", intSz);
 	printf("Size of a long int: %d byte(s)\n", longIntSz);
 	printf("Size of a long long int: %d byte(s)\n", dbleIntSz);
 	printf("Size of a float: %d bytes(s)\n", floatSz);
