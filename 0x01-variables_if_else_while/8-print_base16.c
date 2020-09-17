1dec956ddd753695fa648636c98e9d6a5a4ca7fd
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-17 18:34:41 +0000

    script 8 add

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - prints out the alphabet lowercase a-z using putchar and for loop
+ *Without characters e or q
+ *Return: returns 0 when done
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		putchar(i);
+	}
+	for(i = 97 ; i <=102 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
