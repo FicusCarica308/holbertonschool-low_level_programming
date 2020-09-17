2f892b3cc1d8ebb8adfc8ad8f3ef611ef1d067e4
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-17 14:09:13 +0000

    this commit contains a new project call 0x01-variables_if_else_while and its first script 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *main - gens a random number and stores in the var n, checks if pos,neg, or 0
+ *Return: returns 0 when finished
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+		printf("%i is negative\n", n);
+	else if (n > 0)
+		printf("%i is positive\n", n);
+	else
+		printf("%i is 0\n", n);
+	return (0);
+}
