3a9d88a6858f08748ca2556dbeeb26bcc322e8d9
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-17 14:57:33 +0000

    added a readme with betty fixes to 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ *main - prints out the alphabet lowercase a-z using putchar and for loop
+ *Return: returns 0 when done
+ */
+
+int main(void)
 {
 	char curChar;
-	for(curChar = 'a' ; curChar <='z' ; curChar++)
+
+	for (curChar = 'a' ; curChar <= 'z' ; curChar++)
 	{
 		putchar(curChar);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,5 @@
+Project: 0x01-variable_if_else_while
+Description: This project contains 10 scripts that do various things using
+variables, the if then function, and while/for loops
+Date worked on: 9/17/2020
+Worked on by: Manuel Figueroa
