67ef3020ef1a20bad09d1f67dcbc05e54bcb3054
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-17 19:03:05 +0000

    script 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - prints out the number 00-99 using only putchar and no char var's
+ *with comas and spaces following each one (max 5 putchar's).
+ *Without characters e or q
+ *Return: returns 0 when done
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 100 ; i++)
+	{
+		putchar((i/10)+'0');
+		putchar((i%10)+'0');
+		if (i != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
