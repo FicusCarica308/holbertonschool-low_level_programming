e47146e465cec4b08b865fff667f40f02a3dcab4
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-18 17:18:25 +0000

    contains new script 100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main (void)
+{
+	int i;
+	int c;
+
+	for(i = 48; i <= 56; i++)
+	{
+		for(c = i + 1; c <= 57; c++)
+		{
+			putchar(i);
+			putchar(c);
+			if(i + c <= 112)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
