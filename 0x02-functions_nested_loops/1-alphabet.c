d1f0dc528964018503b37870ca6e1afa95fe3bc0
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-22 13:44:40 +0000

    fix to 1-alphabet.c and my holberton.h header

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "holberton.h"
+
 /**
  *print_alphabet - This functions prints the alphabet
  *Return: returns nothing
  */
 void print_alphabet(void)
 {
-        char c;
+	char c;
 
-        for (c = 'a' ; c <= 'z' ; c++)
-        {
-                _putchar(c);
-        }
-        putchar('\n');
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
