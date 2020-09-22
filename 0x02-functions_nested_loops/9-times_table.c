dd5bc9bfc0223ed371c9c4de50cc6eac64fc2f74
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-22 20:10:36 +0000

    new project directory 0x03-debugging

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,17 +8,38 @@ void times_table(void)
 	int i;
 	int j;
 	int oper;
+	int first;
+	int check = 0;
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
+			check = 0;
 			oper = i * j;
+			if(oper >= 10)
+			{
+				first = oper;
+				while(first >= 10)
+				{
+					first = first / 10;
+				}
+				_putchar(first + '0');
+				oper = oper % 10;
+				_putchar(oper + '0');
+				check++;
+			}else{
 			_putchar(oper + '0');
-			if (j != 9)
+			}
+
+			if (j != 9 && oper < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+			}else
+			{
+				_putchar(',');
 				_putchar(' ');
 			}
 		}
