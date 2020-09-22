63e68ab72e12fe939bf376e05b82767e76de6658
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-22 19:43:13 +0000

    new script 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *add - this adds two integers that are given
+ *@num1: The first integer given
+ *@num2: The second intger given
+ *Return: returns the sum variable (the sum of the two numbers)
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
