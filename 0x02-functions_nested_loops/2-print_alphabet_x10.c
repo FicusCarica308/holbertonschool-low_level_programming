c4586992831bbe353d8ba9202b4d0091c6c2079c
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-22 11:27:00 +0000

    various additions and edits

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *main - calls our print_alphabet_x10 function
+ *Return: function returns 0
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdio.h>
 /**
+ *-----------------------------------------------------------------------
  *_putchar - This functions will print a given char
  *Return: returns the char
  *@c: This parameter holds whatever char the function is called with to be used
@@ -11,7 +12,9 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
 /**
+ *----------------------------------------------------------------------
  *print_alphabet - This functions prints the alphabet
  *Return: returns nothing
  */
@@ -21,8 +24,51 @@ void print_alphabet(void)
 
 	for (c = 'a' ; c <= 'z' ; c++)
 	{
-		putchar(c);
+		_putchar(c);
 	}
 	putchar('\n');
+}
+
+/**
+ *----------------------------------------------------------------------
+ *print_alphabet_x10 - prints the alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for(i = 0; i <= 9 ; i++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+	putchar('\n');
+	}
+}
+
+/**
+ *
+ *
+ */
+int _islower(int c)
+{
+	int i;
+	int check = 0;
+
+	for(i = 97; i <= 122; i++)
+	{
+		if(c == i)
+		{
+			check++;
+		}
+	}
+	if(check == 1)
+	{
+		return (1);
+	}else{
+		return (0);
+	}
 }
 #endif
