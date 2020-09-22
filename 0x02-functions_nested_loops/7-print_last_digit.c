c5ea998c3401b8e321a5f4e1b7a3c87145b3806b
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-22 16:34:56 +0000

    added a new script called 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *print_last_digit - will find the last digit of a number and return it
+ *Return: num after the last digit is assigned to it
+ *@num: The original number that is used to find its last digit
+ */
+int print_last_digit(int num)
+{
+	num = num % 10;
+	return (num);
+}
