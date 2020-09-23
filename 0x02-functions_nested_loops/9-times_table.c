236bd142ce72a71b178fc89be1d70546c0cd1124
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-23 02:00:33 +0000

    hopefully the final fix to script 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,13 +9,11 @@ void times_table(void)
 	int j;
 	int oper;
 	int first;
-	int check = 0;
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
-			check = 0;
 			oper = i * j;
 			if(oper >= 10)
 			{
@@ -24,24 +22,21 @@ void times_table(void)
 				{
 					first = first / 10;
 				}
+				_putchar(',');
+				_putchar(' ');
 				_putchar(first + '0');
 				oper = oper % 10;
 				_putchar(oper + '0');
-				check++;
-			}else{
-			_putchar(oper + '0');
+			} else {
+				if (j > 0)
+				{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				}
+				_putchar(oper + '0');
 			}
 
-			if (j != 9 && oper < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}else
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
 		}
 		_putchar('\n');
 	}
