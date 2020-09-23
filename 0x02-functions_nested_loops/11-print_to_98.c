62f500bdeb546cca02ccd4ff8b2071edd1a9f88e
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-23 18:13:16 +0000

    more betty fixes / fixed a bracket error

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,16 +14,15 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
-		if (i != 98)
-		{
-			printf(", ");
+		        if (i != 98)
+				printf(", ");
+		}
 	} else
 	{
 		for (j = n; j >= 98; j--)
 		{
 			printf("%d", j);
 			if (j != 98)
-			{
 				printf(" ,");
 		}
 	}
