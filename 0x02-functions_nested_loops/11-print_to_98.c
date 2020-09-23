4c5ea72d23f3e5836e4e0b2fde542494c32a3b44
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-23 17:30:58 +0000

    betty format fixes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,24 +9,22 @@ void print_to_98(int n)
 	int i;
 	int j;
 
-	if(n <= 98){
-	for (i = n; i <= 98; i++)
+	if (n <= 98)
 	{
-		printf("%d", i);
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
 		if (i != 98)
 		{
 			printf(", ");
-		}
-	        }
 	} else
 	{
-		for(j = n; j >=98; j--)
+		for (j = n; j >= 98; j--)
 		{
 			printf("%d", j);
 			if (j != 98)
 			{
 				printf(" ,");
-			}
 		}
 	}
 	printf("\n");
