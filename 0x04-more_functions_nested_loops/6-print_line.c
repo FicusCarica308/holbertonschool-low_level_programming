36dcc192287f667785d2440a666f1676db1cc017
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-24 15:46:21 +0000

    completed script 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *print_line - prints a line of underscores specified by n
+ *@n: The number of undersores to print
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			_putchar(95);
+		_putchar('\n');
+	} else
+	{
+		_putchar('\n');
+	}
+}
