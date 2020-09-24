068a7a9655df6c7fa60d2c7802a1c44c0cb5f4c3
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-24 13:55:27 +0000

    completed script 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *mul - this function multiplies two integers
+ *Return: returns the product of the two numbers
+ *@a: the first integer given to the function to be multiplied
+ *@b: the second integer given to the function to be multiplied
+ */
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
