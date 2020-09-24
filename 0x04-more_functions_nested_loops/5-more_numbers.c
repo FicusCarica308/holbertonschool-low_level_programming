6ce3d6150223cf782c81c4cbb3095135ae76c3f4
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-24 18:10:30 +0000

    betty style fixes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,15 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (j = 0; j <=9; j++)
+	for (j = 0; j <= 9; j++)
 	{
 
-	        for (i = 0; i <= 14; i++)
-	        {
-		        if (i >= 10)
-			        _putchar(i/10 + '0');
-		        _putchar(i%10 + '0');
-	        }
-	        _putchar('\n');
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
 	}
 }
