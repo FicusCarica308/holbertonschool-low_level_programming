af35887c3c0f82e0c958259dc5142e4d457c4eef
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-24 17:32:57 +0000

    completed script 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - main will print 0 - 100 and following every 3rd multiple
+ *it will print Buzz Fizz
+ *Return: returns 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+		if (i % 5 == 0 && i % 3 == 0 && i != 0)
+		{
+			printf("FizzBuzz ");
+		} else if (i % 5 == 0 && i != 0)
+		{
+			printf("Buzz ");
+		} else if (i % 3 == 0 && i != 0)
+		{
+			printf("Fizz ");
+		} else
+		{
+			printf("%d ", i);
+		}
+	printf("\n");
+	return (0);
+}
