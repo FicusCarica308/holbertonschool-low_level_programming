81c0262ee1269067f99a1239a3e7485f0321c08e
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-29 14:57:25 +0000

    completed script 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *puts2 - prints every other value of an array
+ *@str: the given string to be printed 
+ */
+void puts2(char *str)
+{
+	int length;
+	int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	length = length - 1;
+
+	for (i = 0; i <= length; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
