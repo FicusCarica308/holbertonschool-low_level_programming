dfc08f2345396e7455784219cc81778fd2e3fa5d
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-29 15:49:10 +0000

    completed script 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *puts_half - prints half of a string
+ *@str: the string to be printed
+ */
+void puts_half(char *str)
+{
+	int length;
+	int i;
+
+	while (str[length] != '\0')
+		length++;
+
+	if (length % 2 != 0)
+	{
+		length--;
+		for (i = length / 2; i <= length; i++)
+			_putchar(str[i]);
+	} else
+	{
+
+		for (i = length / 2; i <= length; i++)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+}
