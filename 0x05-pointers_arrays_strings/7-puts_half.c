d693a213104b95f3a5a83dd33d1feefad27e08d0
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-09-29 16:14:27 +0000

    fixed some string output errors

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,17 +6,17 @@
  */
 void puts_half(char *str)
 {
-	int length;
+	int length = 0;
 	int i;
 
 	while (str[length] != '\0')
 		length++;
-
 	if (length % 2 != 0)
 	{
 		length--;
 		for (i = length / 2; i <= length; i++)
 			_putchar(str[i]);
+		_putchar('\n');
 	} else
 	{
 
