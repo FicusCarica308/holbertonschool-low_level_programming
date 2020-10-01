d79fcd67391c4bd74e765852bd41b7332dfe2657
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-01 16:05:08 +0000

    betty fixes and nested for loop fix in 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,7 +12,8 @@ char *_strcat(char *dest, char *src)
 	int i;
 
 	for (length = 0; dest[length] != '\0'; length++)
-
+	{
+	}
 	for (i = 0; src[i] != '\0'; i++)
 		dest[length + i] = src[i];
 	dest[length + i] = '\0';
