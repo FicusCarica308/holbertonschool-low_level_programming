0eae7c069a9d192806b0d1865967f7d753cbd57c
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-01 23:10:25 +0000

    Completed script 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *cap_string - changes lowercase letters to uppercase
+ *@a: the string to be converted
+ *Return: returns the string completed
+ */
+char *cap_string(char *a)
+{
+	int i = 0;
+	int j = 0;
+	char ascii[] = {' ', '\n', '.', 9, ',', ';', '!',
+			'?', '"', '(', ')', '{', '}' };
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 12; j++)
+		{
+			if (a[i - 1] == ascii[j])
+			{
+				if (a[i] >= 97 && a[i] <= 122)
+				{
+					a[i] = a[i] - 32;
+				}
+			}
+		}
+
+	}
+	return (a);
+}
