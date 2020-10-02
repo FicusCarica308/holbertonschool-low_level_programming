17412a7fbe6470597bec1b407829099faf025796
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-02 17:11:12 +0000

    Added completed script 8-rot13.c with capital implementation

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,27 +7,22 @@
  */
 char *rot13(char *w)
 {
-	int i = 0;
-	int j = 0;
-	char ascii[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-			'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-			'y', 'v', 'w', 'x', 'y', 'z'};
+	int i;
+	int j;
+	char ascii[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	char rot[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'y', 'v', 'w',
-		        'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-		        'h', 'i', 'j', 'k', 'l', 'm'};
+	char rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; w[i] != '\0'; i++)
 	{
-		for (j = 0; j <= 26; j++)
+		for (j = 0; j <= 52; j++)
 		{
 			if (w[i] == ascii[j])
 			{
 				w[i] = rot[j];
+				break;
 			}
 		}
 	}
-	/*	else
-		w[i] = rot[j] - 32;*/
 	return (w);
 }
