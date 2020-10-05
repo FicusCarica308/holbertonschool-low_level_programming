470ec9e94dc22a169872dbc069a93611dbd376b5
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-05 16:28:26 +0000

    Complete script 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,8 +13,14 @@ unsigned int _strspn(char *s, char *accept)
 	int count = 0;
 
 	for (i = 0; s[i] != ' '; i++)
+	{
 		for (j = 0; accept[j] != '\0'; j++)
+		{
 			if (accept[j] == s[i])
+			{
 				count++;
+			}
+		}
+	}
 	return (count);
 }
