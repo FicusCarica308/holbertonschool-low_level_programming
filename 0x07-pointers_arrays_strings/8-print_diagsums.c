e2eb31ba7a1e75304f5ec146896308ecbcc0ac0a
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-05 22:25:58 +0000

    almost complete 8-print_diagsums.c script

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,14 +7,18 @@ void print_diagsums(int *a, int size)
 	int j = 0;
 	int sum = 0;
 	int sum2 = 0;
+	int sizePlus = size*size;
+	int incr1 = size + 1;
+	int sizeSub = size * (size - 1);
+	int incr2 = size - 1;
 
-	for (i = 0; i < (size * size); i = i + (size + 1))
+	for (i = 0; i < sizePlus; i+=incr1)
 	{
 		sum = sum + a[i];
 	}
-	for (j = size * (size - 1); j > 0; j = j - (size - 1))
+	for (j = sizeSub; j > 0; j-=incr2)
 	{
 		sum2 = sum2 + a[j];
 	}
-	printf("%d, %d", sum, sum2);
+	printf("%d, %d\n", sum, sum2);
 }
