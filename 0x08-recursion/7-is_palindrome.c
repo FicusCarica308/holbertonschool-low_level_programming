4ae09da6f137226e5b44bf3523f1894180f61f38
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-08 02:14:38 +0000

    Complete script 7-is_palindrome.c

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,46 +1,27 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- *find_factor - finds if our number is actually a prime factor
- *@n: the number to be checked
- *@a: an increment value (sorta like i in an for loop)
- *Return: returns 0 if prime number and 1 if not a prime number
+ *string_length - will find the length of a given string
+ *@s: the string we are finding the length of
+ *Return: returns the length of a string + the null value
  */
-int test_same(char *s, char *c)
+int string_length(char *s)
 {
-	if (*s != '\0')
-	{
-		if (*s != *c)
-			return (0);
-		else
-			test_same(s + 1, c + 1);
-	}
-	return (1);
-}
-/**
- *_print_rev_recursion - prints the reverse of a string using recursion
- *@s: the string to be reversed
- */
-int reverse_string(char *s, char *c)
-{
-	if (*s != '\0')
-	{
-		reverse_string(s + 1, c + 1);
-		*c = *s;
-	}
-	*c = '\0';
-	return (test_same(s, c));
-}
-/**
- *is_palindrome - will find if a string is a palindrome or not
- *@s: the string we are testing
- *Return: 0 if not a palindrome and 1 if it is
- */
-int is_palindrome(char *s)
-{
-	char c[6] = "hello";
 	if (*s == '\0')
 		return (0);
 	else
-		return (reverse_string(s, c));
+		return (1 + string_length(s + 1));
+}
+/**
+ *is_palindrome - will find if a a given string is a palindrome
+ *@s: the string we are testing
+ *Return: returns 0 if not a palindrome and 1 if it is
+ */
+int is_palindrome(char *s)
+{
+	int length = string_length(s) - 1;
+
+	if (*s == s[length])
+		return (1);
+	return (0);
 }
