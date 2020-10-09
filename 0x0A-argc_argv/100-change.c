a20da694c30fd34152f9039bf55141499f8a2ce9
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-09 13:58:20 +0000

    skeleton of 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - will print out the minimum amount of coins needed to give change
+ *@argc: the amount of arguments passed to the file
+ *@argv: strings of each argument
+ *Return: 0 after completion
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
