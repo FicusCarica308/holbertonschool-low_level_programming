6d121c1943bbf521c96697424f2486b76f35449c
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-09 11:37:27 +0000

    completed script 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - will print out the amount of arguments using argc
+ *@argc: the amount of arguments passed to the file
+ *@argv: strings of each argument
+ *Return: 0 after completion
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
