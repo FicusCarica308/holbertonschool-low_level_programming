204e41fa63cf6930e3b2c39712b4cb53e0d1a524
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-09 11:42:07 +0000

    Complete script 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - will print out all arguments passed to the file using
+ *argc and argv[]
+ *@argc: the amount of arguments passed to the file
+ *@argv: strings of each argument
+ *Return: 0 after completion
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+}
