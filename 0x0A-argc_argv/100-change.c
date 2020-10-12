7232caab8857034115e2d15feb8d31aa3a5ea17c
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-12 00:39:07 +0000

    checker fix test

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	int i = 0;
 	int coins[5] = {25, 10, 5, 2, 1};
 
-	if (argc > 2 || argc == 1)
+	if (argc > 2 || argc < 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	else
 		num = atoi(argv[1]);
 
-	if (num <= 0)
+	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
