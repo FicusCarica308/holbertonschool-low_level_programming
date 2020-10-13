0ff3fbff1b649c951f0c4fd836ac5f6f985bfaed
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-13 17:05:28 +0000

    added a null bite to end of string'

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,12 +16,11 @@ char *_strdup(char *str)
 		return (NULL);
 
 	new = malloc(sizeof(str));
-
 	if (new == NULL)
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
 		new[i] = str[i];
-
+	new[i] = '\0';
 	return (new);
 }
