5510e84916db624643e0187dfa842ba42b7fa3e0
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-13 18:10:11 +0000

    added empty string check

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,8 +22,10 @@ char *str_concat(char *s1, char *s2)
 	unsigned int j = 0;
 	int totalSize = length(s1) + length(s2);
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 == "";
+	if (s2 == NULL)
+		s2 == "";
 
 	str = malloc(sizeof(char) + (1 + totalSize * sizeof(char)));
 
