07dafe21fd3de3f2ddf4c633c242960f50bbe05e
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-14 03:48:31 +0000

    complete script 5-argstostr.c

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -2,21 +2,40 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
- *
- *
- *
+ *argstostr - this function will concat a series of given arguments
+ *@ac: the amount of arguments
+ *@av: the string containing a each string of argument
+ *Return: will return NULL on failure or else a pointer to the new string
  */
 char *argstostr(int ac, char **av)
 {
 	int i = 0;
 	int j = 0;
+	int k = 0;
 	int bites = 0;
 	char *hold;
 
-	for (i = 0; i < ac; i++)
-		for (j = 0; av[i][j] != '\0'; i++)
-			bites++;
-	hold = malloc(sizeof(char) * bites);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-	
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j] != '\0'; j++)
+			bites++;
+
+	hold = malloc((sizeof(char) * bites) + (sizeof(char) * ac));
+
+	if (hold == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			hold[k] = av[i][j];
+			k++;
+		}
+		hold[k] = '\n';
+		k++;
+	}
+	return (hold);
 }
