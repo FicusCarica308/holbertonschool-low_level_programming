800d5845db428e07b431e0b48b587e24a791940d
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-15 22:42:18 +0000

    Complete script 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *_calloc - allocates memory using the calloc function
+ *@nmemb: The amount of elements of the array
+ *@size: The size (data type size) of the array
+ *Return: returns a pointer to the new space
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	unsigned int i = 0;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	for (i = 0; i < nmemb; i++)
+		p[i] = 0;
+
+	if (p == NULL)
+		return (NULL);
+	return (p);
+}
