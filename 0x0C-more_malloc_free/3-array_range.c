64211d397b4ba5ecd7f9dff596e53faa67fb8955
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-16 00:19:10 +0000

    changed the range of memory for the array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,10 +15,10 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	arr = malloc(sizeof(int) * max);
+	arr = malloc(sizeof(int) * (max + 1));
 	if (arr == NULL)
 		return (NULL);
-	for (i = min; i < max; i++)
+	for (i = min; i <= max; i++)
 	{
 		arr[k] = i;
 		k++;
