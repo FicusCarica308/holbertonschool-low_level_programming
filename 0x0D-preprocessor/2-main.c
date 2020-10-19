1b3d0dcee528cbc0f90d031242795bc9fdba83af
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-19 16:21:14 +0000

    fixed betty error

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,7 +4,7 @@
  *main - prints out the name of the file that is called (using __FILE__ macro)
  *Return: returns 0 on success
  */
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
