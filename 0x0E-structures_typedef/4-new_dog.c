f29dff54f15755a3f698db2c908c8715988c174b
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-19 20:15:50 +0000

    test script 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
-/**
- *length - finds the length of a string given
- *@s: the string
- *Return: returns the length of the string
- */
-/*
-int length(char *s)
-{
-	int length = 0;
-
-	while (s[length] != '\0')
-		length++;
-	return (length);
-}
-*/
 /**
  *new_dog - creates a new dog struct
  *@name: the name of the dog
@@ -27,8 +11,6 @@ int length(char *s)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog_dog;
-/*	int length1 = length(name);
-	int length2 = length(owner);*/
 	char *store_owner;
 	char *store_name;
 
