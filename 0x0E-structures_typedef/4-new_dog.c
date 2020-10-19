beadb36963d37319f234f9ac43ed012211206702
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-19 20:08:03 +0000

    test script 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,24 +26,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog_dog;
 	int length1 = length(name);
 	int length2 = length(owner);
+	char *store_owner;
+	char *store_name;
 
 	dog_dog = malloc(sizeof(dog_t));
 	if (dog_dog == NULL)
-		return (NULL);
-
-	owner = malloc(length2 * sizeof(char));
-	if (owner == NULL)
 	{
 		free(dog_dog);
-		free(owner);
 		return (NULL);
 	}
-	name = malloc(length1 * sizeof(char));
+
+	store_owner = malloc(length2 * sizeof(char));
+	if (store_owner == NULL)
+	{
+		free(dog_dog);
+		free(store_owner);
+		return (NULL);
+	}
+	store_name = malloc(length1 * sizeof(char));
 	if (name == NULL)
 	{
 		free(dog_dog);
-		free(owner);
-		free(name);
+		free(store_owner);
+		free(store_name);
 		return (NULL);
 	}
 
