f5f89cafaa9925e1a84a236a7bceabb7cbcd28a2
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-19 18:28:28 +0000

    complete script 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *init_dog - a function to initialize a struct for a dog
+ *@d: a pointer to the previously declared struct
+ *@name: the name of of the dog owner to be put into the struct
+ *@age: the age of the dog to be put into the struct
+ *@owner: the name of the owner to be put into the struct
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
