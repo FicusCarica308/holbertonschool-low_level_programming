b55c605ad77d04e857c040cd29df95bbec069936
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-19 16:20:20 +0000

    complete script using the __FILE__ macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints out the name of the file that is called (using __FILE__ macro)
+ *Return: returns 0 on success
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
