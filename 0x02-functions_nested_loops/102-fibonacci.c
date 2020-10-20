505f89d2d2e4ae3f6f5b0ed95d8678e4e570fd52
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-20 19:00:37 +0000

    advanced task 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *main - prints the first 50 fibonacci numbers
+ *Return: returns 0 when finished
+ */
+int main(void)
+{
+	unsigned long int a = 1;
+	unsigned long int b = 0;
+	int i = 0;
+
+	for (i = 0; i < 25; i++)
+	{
+		b = a + b;
+		printf("%lu\n", a);
+		a = b + a;
+		printf("%lu\n", b);
+	}
+	return (0);
+}
