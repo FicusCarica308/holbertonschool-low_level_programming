b16c2a68e1caba756258be4ef6c5d8923a935f9b
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-21 21:09:30 +0000

    complete c script

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,15 +2,31 @@
 #include <stdlib.h>
 #include "3-calc.h"
 /**
- *
- *
- *
+ *get_op_func - will find which function to return depending on a given
+ * operator
+ *@s: this is the given operator character
+ *Return: returns a pointer to the needed operation
  */
 int (*get_op_func(char *s))(int, int)
 {
-	int check = 0;
-	char *ops = {'+', '-', '*', '/', '%'};
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-	while (check <= 4)
-		p
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (ops[i].op == s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
 }
