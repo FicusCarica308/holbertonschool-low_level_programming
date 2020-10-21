35337eb3247ee9afc324fbe526a3c5f2ee16461d
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-21 17:19:32 +0000

    complete script 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ *print_name - uses a function pointer to print out a persons name using
+ *another function
+ *@name: the name that will be sent to the next function using a callback
+ *@f: the pointer to out function to be called
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
