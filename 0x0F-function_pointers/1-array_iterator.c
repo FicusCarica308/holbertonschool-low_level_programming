ef557cfae738e494660a1fe11d42f161c0c19e11
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-21 17:57:55 +0000

    added betty comments

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
- *
- *
- *
+ *array_iterator - will callback a function for every element of an array
+ *@array: The array that we will cycle through
+ *@size: The size of the array
+ *@action: The function pointer to the given function
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
