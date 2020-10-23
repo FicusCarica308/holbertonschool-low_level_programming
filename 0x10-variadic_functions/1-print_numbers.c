88419f16bf34999cd1450681147a6235c1605894
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-23 14:13:57 +0000

    complete script the prints out numbers spaced by a given string

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *print_numbers - this function prints numbers
+ *@n: # of integers passed to the program
+ *@separator: is the string to be printed between numbers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i = 0;
+
+	if (separator != NULL && n > 0)
+	{
+	va_start(list, n);
+	for (i = 1; i <= n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (i != n)
+			printf("%s", separator);
+	}
+	va_end(list);
+	}
+	printf("\n");
+}
