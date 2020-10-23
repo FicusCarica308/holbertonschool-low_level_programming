458336e333ecda388c03cb4ff8849f1f876e5157
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-23 19:06:41 +0000

    added complete script 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *print_all - prints any given amount of arguments specified by a given format
+ *@format: a string with a list of formats that represent data types c, i, f, s
+ */
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int i = 0;
+	int check = 0;
+	char *null;
+
+	va_start(list, format);
+	while (format[i] != '\0')
+	{
+		check = 0;
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(list, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(list, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(list, double));
+			break;
+		case 's':
+			null = va_arg(list, char*);
+			if (null == NULL)
+				printf("(nil)");
+			else
+				printf("%s", null);
+			break;
+		default:
+			check = 1;
+			break;
+		}
+		if (format[i + 1] != '\0' && check == 0)
+			printf(", ");
+		i++;
+	}
+	va_end(list);
+	printf("\n");
+}
