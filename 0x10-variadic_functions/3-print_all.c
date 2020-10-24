54c8b6f36e0d31df05bcf7d6e68b196128f78bdb
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-24 20:50:49 +0000

    added check in while loop

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -19,13 +19,13 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 		case 'c':
-		        printf("%c", va_arg(list, int));
+			printf("%c", va_arg(list, int));
 			break;
 		case 'i':
 			printf("%d", va_arg(list, int));
 			break;
 		case 'f':
-		        printf("%f", va_arg(list, double));
+			printf("%f", va_arg(list, double));
 			break;
 		case 's':
 			check = va_arg(list, char*);
