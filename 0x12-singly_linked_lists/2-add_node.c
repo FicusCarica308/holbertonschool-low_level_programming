686c94803d637f8671fe1cae8de401c0060c9f34
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-29 17:40:36 +0000

    added complete script 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 /**
  *add_node - this function adds a node to the start of a link list
@@ -10,31 +11,20 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *copy;
 	int strLen = 0;
-	int i = 0;
 	list_t *nwHed;
 
 	nwHed = malloc(sizeof(list_t));
 	if (nwHed == NULL)
 		return (NULL);
 
-	/*for loop to find length of str*/
 	for (strLen = 0; str[strLen] != '\0'; strLen++)
 	{
 	}
 
-	copy = malloc(sizeof(char) * (strLen + 1));
-	if (copy == NULL)
-		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
-		copy[i] = str[i];
-
-	nwHed->str = copy;
+	nwHed->str = strdup(str);
 	nwHed->len = strLen;
 	nwHed->next = *head;
 	*head = nwHed;
-
 	return (*head);
 }
