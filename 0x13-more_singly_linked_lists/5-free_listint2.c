2fb836249a6624506b2531ed674e7d4c3c2d5a25
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-11-02 18:09:28 +0000

    completed script 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - frees an entire linked list and sets the head to NULL
+ * @head: the head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *hold;
+
+	while (*head != NULL)
+	{
+		hold = *head;
+		*head = (*head)->next;
+		free(hold);
+	}
+	*head = NULL;
+}
