c82bf3352b0f3aec1d23eb311121e2dda2383238
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-11-03 23:46:04 +0000

    removed added case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -42,16 +42,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
+
 	if (idx > index_max)
 	{
 		free(newNode);
 		return (NULL);
 	}
+
 	if (idx == 0)
 	{
 		newNode->n = n;
 		newNode->next = *head;
 		*head = newNode;
+		free(newNode);
 		return (*head);
 	}
 
