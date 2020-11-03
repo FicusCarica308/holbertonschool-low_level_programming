1fe3eba490a6f2eebbb3651c778a06200657470a
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-11-03 23:14:43 +0000

    freed index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -49,7 +49,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newNode->n = n;
 		newNode->next = *head;
 		*head = newNode;
-		free(index);
 		return (*head);
 	}
 
