4801b2b08620a9709e68362cf15a62f9c0749634
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-11-03 22:41:52 +0000

    reverted back to old version

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,12 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *hold;
-	listint_t *feed = *head;
 
 	while (*head != NULL)
 	{
-		hold = feed->next;
-		free(feed);
-		feed = hold;
+		hold = *head;
+		*head = (*head)->next;
+		free(hold);
 	}
 	*head = NULL;
 }
