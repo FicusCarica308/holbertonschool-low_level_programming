66618222dba21c584720ded3acbe664e95b1834c
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-11-04 04:02:02 +0000

    added a add to the end if greater than index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -51,7 +51,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newNode->n = n;
 		newNode->next = NULL;
 		index->next = newNode;
-		return (NULL);
+		return (newNode);
 	}
 
 	if (idx == 0)
