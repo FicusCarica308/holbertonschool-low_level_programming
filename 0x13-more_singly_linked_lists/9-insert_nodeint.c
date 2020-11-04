34238d9c97b37878338350fe2c721335de6e194b
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-11-04 00:41:20 +0000

    added or less than 0 to NULL check

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,7 +43,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (newNode == NULL)
 		return (NULL);
 
-	if (idx > index_max)
+	if (idx > index_max || idx < 0)
 	{
 		free(newNode);
 		return (NULL);
