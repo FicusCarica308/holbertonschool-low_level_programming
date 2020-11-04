4fdcd3899faa47693a334090fc9b8f951d9b3068
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-11-04 01:41:52 +0000

    added free after malloc

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,11 +43,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	index = *head;
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
-	{
-		free(newNode);
 		return (NULL);
-	}
-
+	
 	if (idx > index_max)
 	{
 		free(newNode);
