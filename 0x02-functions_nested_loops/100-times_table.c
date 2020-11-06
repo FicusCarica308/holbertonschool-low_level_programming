a1a295eb9d5a9b2f5747d3d8a2bb8c5cacc5e40e
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-11-06 20:06:28 +0000

    almost complete script 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,77 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
+ *
+ *
+ */
+void print_rec(int num)
+{
+	if (num >= 10)
+		print_rec(num / 10);
+	_putchar((num % 10) + '0');
+}
+/**
+ *
+ */
+void print_space(int num)
+{
+	if (num < 10)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+	}
+	if (num >= 10 && num < 100)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+	}
+	if (num > 100)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+}
+/**
+ *
+ */
+void print_num(int num)
+{
+	if (num == 10)
+	{
+		_putchar('1');
+		_putchar('0');
+	}
+	else
+		print_rec(num);
+	print_space(num);
+}
+/**
  *print_times_table - displayes the times tables all the way up to a specified #
  *@n: this parameter holds the value of the given number for the times table
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int oper;
-	int first;
+	int i = 0, j = 0, oper = 0, hold = 0;
 
-	for (i = 0 ; i <= n ; i++)
+	if (n <= 15 && n >= 0)
 	{
-		for (j = 0 ; j <= n ; j++)
+		for (i = 0; i <= n; i++)
 		{
-			oper = i * j;
-			if (oper >= 10)
+			for (j = 0; j <= n; j++)
 			{
-				first = oper;
-				while (first >= 10)
+				oper = i * j;
+				if (oper > 9)
+					print_num(oper);
+				else
 				{
-					first = first / 10;
+					_putchar(oper + '0');
+					print_space(oper);
 				}
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');;
-				_putchar(first + '0');
-				oper = oper % 10;
-				_putchar(oper + '0');
-			} else
-			{
-				if (j > 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				_putchar(oper + '0');
 			}
-
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
