e6ac09118b1b49ac7bb54310353bdc8391979dc7
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-11-06 03:00:53 +0000

    added complete script 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *get_bit - gets a bit at a given index
+ *@n: The number we will be finding the bit of
+ *@index: The location of the bit we want to find from right to left
+ *Return: returns the bit at that location
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int shift, num;
+
+	/*checks if index is greater than 8 bits*/
+	if (index > 63)
+		return (-1);
+
+	shift = n >> index;
+	num = shift & 1;
+	return (num);
+}
