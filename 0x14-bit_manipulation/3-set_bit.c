526560c550a7c7a49e48fa205cecebe5fbb66146
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-11-06 03:10:10 +0000

    added new script

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- *
+ *set_bit - sets the bit of a given index with 1
+ *@n: the number we are adding the bit to
+ *@index: the index we want to insert our number at
+ *Return: returns -1 on failure and 1 if complete
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int op;
+	unsigned long int op;
 
 	if (index > 64)
 		return (-1);
 
-	op = n | 1 << index;
+	op = *n | 1 << index;
 	*n = op;
 	return (1);
 }
