fabb8a69effd7bf6146e378a4e676c642c7956ea
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-11-10 00:54:18 +0000

    fixed betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,7 @@
 int string_length(char *s)
 {
 	int count = 0;
+
 	while (s[count] != '\0')
 		count++;
 	return (count);
@@ -31,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
