fa95b668e98c76949b0d99eacaaa371f0e46a2c1
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-11-10 00:45:33 +0000

    added complete file 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,12 +32,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	length = string_length(text_content);
-
 	fd = open(filename, O_WRONLY | O_CREAT);
 	if (fd == -1)
 		return (-1);
-	if (length != 0)
+	if (text_content != NULL)
+		length = string_length(text_content);
 		for (i = 0; i < length; i++)
 		{
 			print = write(fd, text_content, length);
