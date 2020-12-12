dde11e8684b4bb92c1fe024605f5f86c4cdde280
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-12 18:04:41 +0000

    added if back in

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,8 +10,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_head;
 
 	new_head = malloc(sizeof(dlistint_t));
-	/*if (new_head == NULL)
-		return (NULL);*/
+	if (new_head == NULL)
+		return (NULL);
 	new_head->n = n;
 	new_head->next = *head;
 	new_head->prev = NULL;
