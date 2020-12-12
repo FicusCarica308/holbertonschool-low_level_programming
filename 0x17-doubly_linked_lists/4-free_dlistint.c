d6c27a673ab300c9fca5ad1ac04aca9254a83477
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-12 18:47:24 +0000

    betty formatted code

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,12 +1,16 @@
 #include "lists.h"
+/**
+ *free_dlistint - frees a given linked list
+ *@head: The current head of the given linked list
+ */
 void free_dlistint(dlistint_t *head)
 {
-        dlistint_t *free_head;
+	dlistint_t *free_head;
 
-        while (head != NULL)
-        {
-                free_head = head;
-                head = head->next;
-                free(free_head);
-        }
+	while (head != NULL)
+	{
+		free_head = head;
+		head = head->next;
+		free(free_head);
+	}
 }
