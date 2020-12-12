0b46c5f75e698ac4c597bddc508cae8922af6a9e
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-12 22:50:41 +0000

    added betty formated code to delete a node at an index

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ *delete_dnodeint_at_index - deletes a node at a given index
+ *@head: the current head of the linked list
+ *@index: the index we want to remove the node in at
+ *Return: 1 if free/removal is succseful and -1 if not
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *temp_head = *head;
+
+
+	if (temp_head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		if (temp_head->next != NULL)
+			temp_head->next->prev = NULL;
+		*head = temp_head->next;
+		free(temp_head);
+		return (1);
+	}
+
+	while (temp_head != NULL)
+	{
+		if (i == index)
+		{
+			temp_head->prev->next = temp_head->next;
+			temp_head->next->prev = temp_head->prev->next;
+			free(temp_head);
+			return (1);
+		}
+		i++;
+		temp_head = temp_head->next;
+	}
+	if (index > i)
+		return (-1);
+	temp_head->prev->next = NULL;
+	free(temp_head);
+	return (1);
+}
