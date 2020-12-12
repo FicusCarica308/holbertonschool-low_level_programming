aa653319d25395f1db679126d2655169a279e82e
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-12 20:32:52 +0000

    added the complete script without betty for testing

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+    unsigned int i = 0;
+    dlistint_t *new_node , *temp_head = *h;
+
+    if (idx == 0)
+        return (add_dnodeint(&*h, n));
+	while (temp_head != NULL)
+	{
+		if (i == idx)
+        {
+            new_node = malloc(sizeof(dlistint_t));
+            if (new_node == NULL)
+                return (NULL);
+            new_node->n = n;
+            new_node->next = temp_head;
+            new_node->prev = temp_head->prev;
+            temp_head->prev->next = new_node;
+            temp_head->prev = new_node;
+            return (new_node);
+        }
+		i++;
+		temp_head = temp_head->next;
+	}
+    return (add_dnodeint_end(&*h, n));
+}
