4d0fd4453f08d64007a935314164f48bebb3098a
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-12 19:07:29 +0000

    complete script to find node at index with betty format

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,18 +1,23 @@
 #include "lists.h"
-
+/**
+ *get_dnodeint_at_index - gets node at a given index (index we move head to)
+ *@head: the current head of the linked list
+ *@index: the given index of the linked list to find the node
+ *Return: returns node at the given index
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    while (head != NULL)
-    {
-        if (i == index)
-            return (head);
-        i++;
-        head = head->next;
-    }
-    return (NULL);
+	while (head != NULL)
+	{
+		if (i == index)
+			return (head);
+		i++;
+		head = head->next;
+	}
+	return (NULL);
 }
