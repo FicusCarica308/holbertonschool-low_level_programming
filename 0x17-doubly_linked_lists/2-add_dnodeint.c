8cff11247d1614f9218b67e62d736f4a6153dd01
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-12 00:27:11 +0000

    added script to add a new head to the doubly linked list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *add_dnodeint - adds a new head node to a doubly linked list
+ *@head: the current head of the list
+ *@n: the new number to be insert as the data for the node
+ *Return: returns the new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_head;
+
+	new_head = malloc(sizeof(dlistint_t));
+	if (new_head == NULL)
+		return (NULL);
+	new_head->n = n;
+	new_head->next = *head;
+	new_head->prev = NULL;
+	*head = new_head;
+	return (*head);
+}
