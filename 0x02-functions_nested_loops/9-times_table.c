5ea407f8973d29021e885e29a9833bbfc0046909
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-15 02:58:50 +0000

    added all modifcations and deletes to directory to resolve git status changes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,23 @@ void times_table(void)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
-			oper = i * j;
+			oper unsigned long int num = 0;
+			unsigned long int num2 = 1;
+			unsigned long int hold = 0;
+			int i = 0;
+
+			for (i = 0; i <= 51; i++)
+			{
+				if (i > 1)
+					printf("%lu", num);
+				if (i != 51 && i > 1)
+					printf(", ");
+				hold = num + num2;
+				num = num2;
+				num2 = hold;
+			}
+			printf("\n");
+			return (0);= i * j;
 			if (oper >= 10)
 			{
 				first = oper;
