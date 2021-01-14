ae815d61bc205ac40b969b3f5e906cc0ac292382
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-14 17:36:29 +0000

    added betty formatting to complete function

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,14 +1,16 @@
 #include "hash_tables.h"
 
 /**
-*
-*
-*
+*key_index - finds a random index for our hash table that
+*we can place a new key into
+*@key: The key that we will use the djb2 algorithm on
+*@size: the size of our hash array which we will mod the djb2 output to
+*Return: will return 0 on failure and the new index on success
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    if (key == NULL)
-        return 0;
+	if (key == NULL)
+		return (0);
 
-    return (hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
