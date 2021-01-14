edc0c4a17fb1c86b0081e29fb151e6cf85633fa8
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-14 17:15:34 +0000

    added more protos to header and better allocation in task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,18 +6,21 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_node_t **array[1024];
+    hash_node_t **array;
     hash_table_t *new_table = NULL;
     unsigned int index = 0;
 
     new_table = malloc(sizeof(hash_table_t));
     if (new_table == NULL)
         return NULL;
+    array = malloc(sizeof(hash_node_t) * 1024);
+        if (array == NULL)
+            return NULL;
     
     for (index = 0; index < 1023; index++)
         array[index] = NULL;
     
-    new_table->array = *array;
+    new_table->array = array;
     new_table->size = size;
 
     return (new_table);
