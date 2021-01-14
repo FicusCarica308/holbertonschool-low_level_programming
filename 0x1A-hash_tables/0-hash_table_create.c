421ec3f03720e09d55b3d92f3d80ec66907ea336
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-14 16:06:04 +0000

    added a function to init a hash table. doesnt fill array with pointer to node yet

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+*hash_table_t - this function creates a hash table from a given size
+*@size: size is the size of the hash table we are going to create
+*Return: returns a pointer to the newly created hash table or NULL if failed
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_node_t **array[1024];
+    hash_table_t *new_table = NULL;
+    unsigned int index = 0;
+
+    new_table = malloc(sizeof(hash_table_t));
+    if (new_table == NULL)
+        return NULL;
+    
+    for (index = 0; index < 1023; index++)
+        array[index] = NULL;
+    
+    new_table->array = *array;
+    new_table->size = size;
+
+    return (new_table);
+}
