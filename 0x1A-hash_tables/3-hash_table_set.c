51b94e34c7225bfac1ce5469236d0affa7b07f1c
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-01-16 03:55:20 +0000

    added some new checks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,8 +41,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (key == NULL || ht == NULL || key[0] == '\0')
 		return (0);
-	if (ht->array == NULL || ht->size == 0)
-		return(0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	dup_value = strdup(value);
