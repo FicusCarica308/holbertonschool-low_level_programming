43b764b576524028dda9e2a46b43b5a1561d77a1
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-02-20 00:08:47 +0000

    added improved version of code after diagnal error

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,28 +4,26 @@
 
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
-    sum = 0
-    count = 0
-    cool = 0
-    grid2 = [row[:] for row in grid]
-
-    for column in grid:
-        if 1 not in column:
-            grid2.remove(list(column))
-            count -= 1
-        elif 1 in column:
-            for i in column:
-                if i == 0:
-                    grid2[count].remove(i)
-        count += 1
-
-    sum = len(grid2) * 2
-    sum += len(grid2[0]) + len(grid2[len(grid2) - 1])
-
-    for i in range(0, len(grid2) - 1):
-        cool = len(grid2[i])
-        cool = cool - len(grid2[i + 1])
-        if cool < 0:
-            cool = cool * -1
-        sum += cool
-    return sum
+    total = 0
+    for column in range(0, len(grid)):
+        for row in range(0, len(grid[column])):
+            # if index number is 1 instead of 0
+            if grid[column][row] == 1:
+                total += 4
+                # checks the number to the left
+                if row != 0:
+                    if grid[column][row - 1] == 1:
+                        total -= 1
+                # checks the number to the right
+                if row != len(grid[column]) - 1:
+                    if grid[column][row + 1] == 1:
+                        total -= 1
+                # checks the bottom list position
+                if column != 0:
+                    if grid[column - 1][row] == 1:
+                        total -= 1
+                # checks the bottom list position:
+                if column != len(grid) - 1:
+                    if grid[column + 1][row] == 1:
+                        total -= 1
+    return total
