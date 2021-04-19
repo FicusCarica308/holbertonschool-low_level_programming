6601ca38f551eaf18f0cfcf4223539cded349013
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-19 23:17:03 +0000

    added header file with mandatory task protos

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+/*librarys*/
+#include <stdio.h> /*can only use printf*/
+
+/*function prototypes*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
