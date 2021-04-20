6141486cad534f0a3c28b666e8a0bec46631ad11
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-20 00:02:47 +0000

    added task 1 binary search function

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+/**
+* binary_search - function that sorts a least to greatest array
+*@size: The size of the given array
+*@array: The array that will be searched
+*@value: The value we are searching for
+*Return: returns the index or -1 on failure
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	unsigned int index = 0, index_2 = 0;
+	int sub_index;
+
+	if (array == NULL)
+		return (-1);
+	size--;
+
+	while (index <= size)
+	{
+		/*prints array*/
+		printf("searching in array: ");
+		for (index_2 = index; index_2 <= size; index_2++)
+		{
+			printf("%d", array[index_2]);
+			if (index_2 != size)
+				printf(", ");
+		}
+		printf("\n");
+
+		/*sorts*/
+		sub_index = (index + size) / 2;
+		if (array[sub_index] < value)
+			index = sub_index + 1;
+		else if (array[sub_index] > value)
+			size = sub_index - 1;
+		else
+			return (sub_index);
+	}
+	return (-1);
+}
