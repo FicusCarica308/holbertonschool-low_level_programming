44a6abda91e3a4ee04ab5d83c897f92c29a337d4
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-21 01:18:44 +0000

    added printf

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,6 +16,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (index = 0; index < size; index++)
 	{
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
 		if (array[index] == value)
 			return (index);
 	}
